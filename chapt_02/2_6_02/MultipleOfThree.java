88d739f83ab4ee56d712957ec9c5e1953c23a802
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-07-08 18:18:56 +0900

    Chapter 2

---
+++ chapt_02/2_6_02/MultipleOfThree.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultipleOfThree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("수를 입력하시오: ");
+        int number = scanner.nextInt(); // input integers
+        if (number % 3 == 0) {
+            System.out.println("3의 배수 입니다.");
+
+        }
+
+        else {
+            System.out.println("3의 배수가 아닙니다. ");
+        }
+
+        scanner.close();
+    }
+}
