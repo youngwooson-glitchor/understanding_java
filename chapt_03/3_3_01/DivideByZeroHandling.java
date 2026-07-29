8dc0a748ede1b92a93b97aada7c4f88e30466f84
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-07-29 17:26:09 +0900

    Chapter 3 (#5)
    * chapt3 temp
    
    * Chapter 3

---
+++ chapt_03/3_3_01/DivideByZeroHandling.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DivideByZeroHandling {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("나뉨수를 입력하시오: ");
+
+            int dividend = scanner.nextInt();
+
+            System.out.print("나눗수를 입력하시오: ");
+
+            int divisor = scanner.nextInt();
+
+            try { 
+                System.out.println(dividend + "를 " + divisor + "로 나누면 몫은 " + dividend/divisor + "입니다.");
+
+                break;
+            } catch (ArithmeticException e) {
+                System.out.println("0으로 나눌 수 없습니다! 다시 입력하세요!");
+            }
+
+
+
+        }
+
+        scanner.close();
+
+
+    }
+    
+}
