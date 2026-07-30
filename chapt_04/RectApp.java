d7dcd2a9ef4a34fed87abbf322927a4d8e5fb729
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-07-30 13:58:35 +0900

    chapt 4

---
+++ chapt_04/RectApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Rectangle {
+    int width, height;
+
+    public int getArea() {
+        return width * height;
+    }
+}
+
+
+public class RectApp {
+    public static void main(String[] args) {
+
+        Rectangle rect = new Rectangle();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(">> ");
+        rect.width = scanner.nextInt();
+        rect.height = scanner.nextInt();
+        System.out.println("사각형의 면적은 " + rect.getArea());
+        scanner.close();
+
+    }
+}
