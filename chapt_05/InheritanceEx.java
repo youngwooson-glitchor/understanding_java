3d6f0c31c0a5003afa26c1a8d9fed1d7321b9392
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-07-31 09:57:25 +0900

    Chapter 5 (#7)

---
+++ chapt_05/InheritanceEx.java
@@ -0,0 +1,40 @@
+package chapt_05;
+
+class Person {
+    private int weight;
+    int age;
+    protected int height;
+    public String name;
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
+
+
+class Student extends Person {
+    public void set() {
+        age = 30;
+        name = "홍길동";
+        height = 175;
+        // weight = 99;
+        setWeight(99);
+    }
+
+}
+
+
+
+public class InheritanceEx {
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.set();
+
+    }
+}
