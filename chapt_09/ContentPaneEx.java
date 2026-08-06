38b81f8a2f253ff046cd582ba031a4b76887a4e4
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-08-06 13:07:02 +0900

    Chapter 9 (#11)

---
+++ chapt_09/ContentPaneEx.java
@@ -0,0 +1,29 @@
+package chapt_09;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPaneEx extends JFrame {
+
+    public ContentPaneEx() {
+        setTitle("ContentPane과 JFrame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = getContentPane();
+        contentPane.setBackground(Color.ORANGE);
+        contentPane.setLayout(new FlowLayout());
+
+        contentPane.add(new JButton("OK"));
+        contentPane.add(new JButton("Cancel"));
+        contentPane.add(new JButton("Ignore"));
+
+        setSize(300, 150);
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new ContentPaneEx();
+
+    }
+}
