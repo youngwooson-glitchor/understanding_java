91f672e4589ac01d49d8ef9a9cab9d1950437e49
youngwooson-glitchor <youngwoo.son@glitchor.com>
2026-08-06 10:37:53 +0900

    Chapter 7 (#9)

---
+++ chapt_07/GenericMethodEx.java
@@ -0,0 +1,36 @@
+package chapt_07;
+
+
+
+public class GenericMethodEx {
+    public static <T> GStack<T> reverse(GStack<T> a) {
+        GStack<T> s = new GStack<T>();
+
+        while (true) {
+            T tmp;
+
+            tmp = a.pop();
+            if (tmp == null) {
+                break;
+            } else {
+                s.push(tmp);
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        GStack<Double> gs = new GStack<Double>();
+
+        for (int i = 0; i < 5; i++) {
+            gs.push(new Double(i));
+        }
+
+        gs = reverse(gs);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(gs.pop());
+        }
+
+    }
+}
